c256de99bb9365b3a9c10837a3be7d7932756c46
Franco Arto <francoarto@gmail.com>
2018-10-26 02:36:55 -0300

    Ex.2

--- 03-HTML5-APIs/scripts/main.js
+++ 03-HTML5-APIs/scripts/main.js
@@ -79,3 +79,40 @@ function clearData()
     console.log('IndexedDB not supported by browser');
   }
 }
+
+//Drag and drop files
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  
+  if (event.dataTransfer.items.length > 1) {
+    console.log('Max. 1 simultaneous file');
+  }
+  else { 
+    if (event.dataTransfer.items[0].kind === 'file') { //Check if dropped items are files
+      var file = event.dataTransfer.items[0].getAsFile();
+      console.log(file.name);
+
+      var reader = new FileReader();
+
+      reader.readAsText(file);
+
+      reader.onload = ev => {
+        data = reader.result;
+      }
+    
+      reader.onloadend = ev => {
+        event.target.innerHTML = data;
+      }
+    
+      reader.onerror = ev => {
+        console.log('Reading error');
+      }      
+    }
+  }
+
+    
+}
