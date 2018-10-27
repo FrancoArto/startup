ec9f40a56d0c9760275485b917c46493fd214e94
Franco Arto <francoarto@gmail.com>
2018-10-27 01:19:56 -0300

    Ex. 3

--- 03-HTML5-APIs/scripts/main.js
+++ 03-HTML5-APIs/scripts/main.js
@@ -112,7 +112,28 @@ function drop(event) {
         console.log('Reading error');
       }      
     }
+  }    
+}
+
+//Testing web socket
+function testWebSocket(msg) {
+  var ws = new WebSocket('wss://echo.websocket.org/');
+
+  ws.onopen = event => {
+    console.log('Connected');
+    ws.send(msg);
   }
 
-    
+  ws.onclose = event => {
+    console.log('Disconnected');
+  }
+
+  ws.onmessage = event => {
+    console.log('Receiving message');
+    document.getElementById('wsOutput').innerHTML = 'Response ' + event.data;
+  }
+
+  ws.onerror = event => {
+    console.log('WebSocket error');
+  }  
 }
