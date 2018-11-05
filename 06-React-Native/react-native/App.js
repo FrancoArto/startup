cc69767b7b6be9bc6c30ad8ee26a7bab7828a895
Franco Arto <francoarto@gmail.com>
2018-11-05 01:29:01 -0300

    Topic 1 Ex. 3

--- 06-React-Native/react-native/App.js
+++ 06-React-Native/react-native/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
+      <ScrollView style={{
+        flex: 1
+        }} horizontal= {true}>
         <View
           style={{
             flexDirection: 'row',
-            height: 100,
+            width: 400,
+            height: 1000,
             padding: 12,
             backgroundColor: 'red'
           }}>
@@ -16,6 +20,7 @@ export default class App extends React.Component {
         <View
           style={{
             flexDirection: 'row',
+            width: 300,
             height: 60,
             padding: 12,
             backgroundColor: 'yellow'
@@ -24,12 +29,13 @@ export default class App extends React.Component {
         <View
           style={{
             flexDirection: 'row',
+            width:200,
             height: 300,
             padding: 12,
             backgroundColor: 'blue'
           }}>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
