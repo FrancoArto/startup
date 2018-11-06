da3e3a6a261b987a8b7bef39249e36e3617aad19
Franco Arto <francoarto@gmail.com>
2018-11-06 15:44:06 -0300

    Topic 3 Ex. 3

--- 06-React-Native/react-native/components/containers/EventScreen/EventScreen.js
+++ 06-React-Native/react-native/components/containers/EventScreen/EventScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import styles from '../../../styles';
+import AppTextInput from '../../presentational/AppTextInput/AppTextInput';
 
 export default class EventScreen extends React.Component {
   constructor(props) {
@@ -24,7 +25,7 @@ export default class EventScreen extends React.Component {
     return (
       <View style={styles.backgroundRow}>
         <TouchableOpacity onPress={this.handleOnPress}>
-          <TextInput style={styles.textInput} placeholder="Insert text here!" onChangeText={(text) => {this.setState( { text } )}} value={this.state.text} />
+          <AppTextInput type="password" style={styles.textInput} onChangeText={(text) => {this.setState( { text } )}} value={this.state.text} />
         </TouchableOpacity>
       </View>
     );
