5427933915a8d27b6ed7fafebfdd8e953e4049b6
Franco Arto <francoarto@gmail.com>
2018-11-07 01:16:15 -0300

    Topic 4 Ex. 7

--- 06-React-Native/topic4-6/components/containers/Forms3.js
+++ 06-React-Native/topic4-6/components/containers/Forms3.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import styles from '../../styles';
+import AppTouchableOpacity from '../presentational/AppTouchableOpacity';
 
 class Forms3 extends React.Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.touchableContainer}>
         <Text>Forms 3</Text>
+        <AppTouchableOpacity type="primary" value="Finish" onPress={() => this.props.navigation.navigate('Forms')} />
       </View>
     );
   }
