ab424d48680e7d823a8c3eed844105cee16ee28e
Franco Arto <francoarto@gmail.com>
2018-11-06 21:22:01 -0300

    Topic 4 Ex.4

--- 06-React-Native/topic4-6/components/containers/MainScreen.js
+++ 06-React-Native/topic4-6/components/containers/MainScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 import SecondaryScreen from './SecondaryScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 
 class MainScreen extends React.Component {
@@ -17,6 +18,23 @@ class MainScreen extends React.Component {
 
 
 export default createBottomTabNavigator ({
-  Main: MainScreen,
-  Secondary: {screen : SecondaryScreen}
+  Forms: MainScreen,
+  Other: {screen : SecondaryScreen}
+}, {
+  navigationOptions : ({navigation}) => ({
+    tabBarIcon: ({focused, tintColor}) => {
+      const { routeName } = navigation.state;
+      var iconName;
+      if (routeName === 'Forms') {
+        iconName = 'ios-list';
+      } else if (routeName === 'Other') {
+        iconName = 'ios-more';
+      }
+      return <Ionicons name={iconName} size={20} color={tintColor}/>;
+    },
+    tabBarOptions: {
+      activeTintColor: 'green',
+      inactiveTintColor: 'grey'
+    }
+  })
 });
